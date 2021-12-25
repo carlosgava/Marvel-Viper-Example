fcf31506c8fdc8ab3a26612cbbe81fbca8304450
Carlos Henrique Gava <carlos.gava@gmail.com>
2021-12-25 00:09:39 -0300

    Update AppConstants.swift

--- Marvel VIPER Example/AppConstants.swift
+++ Marvel VIPER Example/AppConstants.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 struct AppConstants {
-    static let publicKey :String = "66ecea385061b529619af4da0158f307"
-    static let privateKey:String = "17fbb115a43140e8e51bbec66d9d03aa1348342d"
+    static let publicKey :String = ""
+    static let privateKey:String = ""
 }
